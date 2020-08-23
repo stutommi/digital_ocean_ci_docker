48b1778256d9ba0f3b7528d97e26c74711a3dd6c
stutommi <tommi.teetee@hotmail.com>
2020-08-23 22:31:56 +0300

    add front build step

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ IMAGE="stutommi/do-test"                             # Docker image
 GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git hash and tags
 
 # Build and tag image
+yarn build
 docker build -t ${IMAGE}:${GIT_VERSION} .
 docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 
